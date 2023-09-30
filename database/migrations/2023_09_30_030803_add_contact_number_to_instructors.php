677e18051d01f6acf89898e473bc595baebc7f5b
ninagayy <ninag6257@gmail.com>
2023-09-30 11:10:27 +0800

    added activity # 5

---
+++ database/migrations/2023_09_30_030803_add_contact_number_to_instructors.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddContactNumberToInstructors extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('instructors', function (Blueprint $table) {
+            $table->string('contact_number', 11)->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('instructors', function (Blueprint $table) {
+            //
+        });
+    }
+}
